cbe1a769adcfd24cd7ad364050e7ce817753402b
ssuico <ssuico@outdoorequipped.com>
2025-06-03 11:06:14 +0800

    fa error build fixed

--- portfolio-2025/src/layout/Navbar.tsx
+++ portfolio-2025/src/layout/Navbar.tsx
@@ -1,19 +1,12 @@
-
-import { FaFacebook } from "react-icons/fa";
+import { FaFacebook, FaGithub, FaInstagram } from "react-icons/fa6";
 import { FaSquareXTwitter } from "react-icons/fa6";
-import { FaGithub } from "react-icons/fa";
-import { FaInstagram } from "react-icons/fa";
-import { FaCode } from "react-icons/fa";
-
-import hitoru from '../assets/hitoruLogo1.png'
 
 const Navbar = () => {
   return (
     <div className='w-[80%] h-[5rem] bg-black flex flex-row justify-around items-center text-white fixed top-10 left-1/2 -translate-x-1/2 z-50 rounded-lg opacity-80'>  
-    <div className="w-[10rem] h-[4rem] overflow-hidden rounded-lg flex flex-row items-center justify-center ">
-      <p className="text-2xl font-bold">HITORU</p>
-      <FaCode className="text-2xl font-bold"/>
-    </div>
+      <div className="w-[10rem] h-[4rem] overflow-hidden rounded-lg flex flex-row items-center justify-center ">
+        <p className="text-2xl font-bold">HITORU</p>
+      </div>
       <div className='flex flex-row gap-8 text-xl'>
         <a href="#home" className='font-semibold'>Home</a>
         <a href="#about" className='font-semibold'>About</a>
@@ -21,10 +14,10 @@ const Navbar = () => {
         <a href="#contact" className='font-semibold'>Contact</a>
       </div>
       <div className='flex flex-row gap-4'>
-        <FaFacebook className="h-5 w-5 cursor-pointer"/>
-        <FaSquareXTwitter className="h-5 w-5 cursor-pointer"/>
-        <FaInstagram className="h-5 w-5 cursor-pointer"/>
-        <FaGithub className="h-5 w-5 cursor-pointer"/>
+        <span className="h-5 w-5 cursor-pointer"><FaFacebook /></span>
+        <span className="h-5 w-5 cursor-pointer"><FaSquareXTwitter /></span>
+        <span className="h-5 w-5 cursor-pointer"><FaInstagram /></span>
+        <span className="h-5 w-5 cursor-pointer"><FaGithub /></span>
       </div>
     </div>
   )
